11c72c272872c03b26172d62237f73b8a9c344d8
nobolaa <nabilaliasserhaou@gmail.com>
2022-05-11 20:54:14 +0200

    Installed twig package. Created index view and used conditionals on it. Showed view through the controller.

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -11,9 +11,6 @@ class MoviesController extends AbstractController
     #[Route('/movies/{name}', name: 'app_movies', defaults:['name' => null], methods:['GET', 'HEAD'])]
     public function index($name): Response
     {
-        return $this->json([
-            'message' => $name,
-            'path' => 'src/Controller/MoviesController.php',
-        ]);
+        return $this->render('index.html.twig', compact('name'));
     }
 }
