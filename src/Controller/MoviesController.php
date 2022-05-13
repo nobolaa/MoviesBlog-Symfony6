1d17fabd1ebf367e93c786b683efa7e712952432
nobolaa <nabilaliasserhaou@gmail.com>
2022-05-13 22:44:10 +0200

    Added assets package. Added encore package and ran npm install command. Added styles for background and h1 elements. Showed movies gotten from the controller on the view.

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -24,24 +24,6 @@ class MoviesController extends AbstractController
         //findAll() - SELECT * FROM movies;
         $movies = $repository->findAll();
 
-        //find() - SELECT * FROM movies WHERE id = 5;
-        $movies = $repository->find(5);
-
-        //findBy() - SELECT * FROM movies ORDER BY id DESC;
-        $movies = $repository->findBy([], ['id' => 'DESC']);
-
-        /*findOneBy() - SELECT * FROM movies 
-                               WHERE releaseYear = 2008 AND title = 'The Dark Kinght' 
-                               ORDER BY id DESC;*/        
-        $movies = $repository->findOneBy(['releaseYear' => 2008, 'title' => 'The Dark Kinght'], ['id' => 'DESC']);
-
-        //count() - SELECT COUNT() FROM movies WHERE id = 5;
-        $movies = $repository->count(['id' => 10]);
-
-        $movies = $repository->getClassName();
-
-        dd($movies);
-
-        //return $this->render('index.html.twig', compact('movies'));
+        return $this->render('index.html.twig', compact('movies'));
     }
 }
