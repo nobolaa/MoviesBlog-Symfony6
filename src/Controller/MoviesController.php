54ebc0ff371e51f371cdd64df20c0b15c57e7006
nobolaa <nabilaliasserhaou@gmail.com>
2022-05-14 23:30:16 +0200

    Added dynamic data gotten from the database on vies index and show of movies.

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Movie;
+use App\Repository\MovieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,20 +11,27 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    private $em;
+    private $movieRepository;
 
-    public function __construct(EntityManagerInterface $em){
-        $this->em = $em;
+    public function __construct(MovieRepository $movieRepository){
+        $this->movieRepository = $movieRepository;
     }
 
-    #[Route('/movies', name:'movies')]
+    #[Route('/movies', methods:['GET'], name:'movies')]
     public function index(): Response
     {
-        $repository = $this->em->getRepository(Movie::class);
         
         //findAll() - SELECT * FROM movies;
-        $movies = $repository->findAll();
-
+        $movies = $this->movieRepository->findAll();
+        
         return $this->render('movies/index.html.twig', compact('movies'));
+    }
+
+    #[Route('/movies/{id}', methods:['GET'], name:'show')]
+    public function show($id): Response
+    {
+        $movie = $this->movieRepository->find($id);
+        
+        return $this->render('movies/show.html.twig', compact('movie'));
     }
 }
