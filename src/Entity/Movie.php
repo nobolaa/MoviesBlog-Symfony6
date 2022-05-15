3c9abda6ee0f7d6477f4af84cf0e8350456827a7
nobolaa <nabilaliasserhaou@gmail.com>
2022-05-16 01:53:16 +0200

    Added validations on creating and editing a movie:     - Deleted required attribute on formType of movie.     - Added validations on movie entity.     - Installed package for validations

--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
+use Symfony\Component\Validator\Constraints as Assert;
 #[ORM\Entity(repositoryClass: MovieRepository::class)]
 class Movie
 {
@@ -15,13 +16,20 @@ class Movie
     #[ORM\Column(type: 'integer')]
     private $id;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'string', length: 255)]/**
+     * @Assert\NotBlank
+     * @Assert\Length(min=3)
+     */
     private $title;
 
-    #[ORM\Column(type: 'integer')]
+    #[ORM\Column(type: 'integer')]/**
+    * @Assert\NotBlank
+    */
     private $releaseYear;
 
-    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]/**
+    * @Assert\NotBlank
+    */
     private $description;
 
     #[ORM\Column(type: 'string', length: 255)]
@@ -45,7 +53,7 @@ class Movie
         return $this->title;
     }
 
-    public function setTitle(string $title): self
+    public function setTitle(?string $title): self
     {
         $this->title = $title;
 
@@ -57,7 +65,7 @@ class Movie
         return $this->releaseYear;
     }
 
-    public function setReleaseYear(int $releaseYear): self
+    public function setReleaseYear(?int $releaseYear): self
     {
         $this->releaseYear = $releaseYear;
 
@@ -81,7 +89,7 @@ class Movie
         return $this->imagePath;
     }
 
-    public function setImagePath(string $imagePath): self
+    public function setImagePath(?string $imagePath): self
     {
         $this->imagePath = $imagePath;
 
