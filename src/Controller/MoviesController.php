549e08ed58118d42972a09e959a9b86fc16058bc
nobolaa <nabilaliasserhaou@gmail.com>
2022-05-16 01:23:17 +0200

    Added functionality to edit movies:     - Unlinked property image from model on fromtype of movie.     - Used symfony forms on edit view.     - Added logic to show edit page and update a movie on controller.     - Created button on show view to edit the movie.

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -7,6 +7,7 @@ use App\Form\MovieFormType;
 use App\Repository\MovieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -75,5 +76,49 @@ class MoviesController extends AbstractController
         $movie = $this->movieRepository->find($id);
         
         return $this->render('movies/show.html.twig', compact('movie'));
+    }
+
+    
+    #[Route('/movies/edit/{id}', methods:['GET', 'POST'], name:'edit_movie')]
+    public function edit($id, Request $request): Response
+    {
+        $movie = $this->movieRepository->find($id);
+        $form = $this->createForm(MovieFormType::class, $movie);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $imagePath = $form->get('imagePath')->getData();
+            if($imagePath){
+                $oldPath = $this->getParameter('kernel.project_dir') . '/public' . $movie->getImagePath();
+                if($movie->getImagePath() !== null && file_exists($oldPath)){    
+                    unlink($oldPath);
+                }
+
+                $newFileName = uniqid() . '.' . $imagePath->guessExtension();
+                try {
+                    $imagePath->move(
+                        $this->getParameter('kernel.project_dir') . '/public/uploads',
+                        $newFileName
+                    );
+                } catch (FileException $e) {
+                    return new Response($e->getMessage());
+                }
+
+                $movie->setImagePath('/uploads/' . $newFileName);  
+            }
+                
+            $movie->setTitle($form->get('title')->getData());
+            $movie->setReleaseYear($form->get('releaseYear')->getData());
+            $movie->setDescription($form->get('description')->getData());
+            
+            $this->em->flush();
+
+            return $this->redirectToRoute('movies');
+        }
+        
+        return $this->render('movies/edit.html.twig', [
+            'movie' => $movie,
+            'form' => $form->createView()
+        ]);
     }
 }
